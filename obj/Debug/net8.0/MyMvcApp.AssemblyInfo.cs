adeaf8343a5fc616357b1307497085efea07ff56
Ivelin-uk <ivoagaininwow@gmail.com>
2025-04-06 10:10:27 +0300

    add

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40c6f55ab631512da57159e732024d0d9edca58b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e02d9f538e916ae43492882bc854f6fe98872f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
